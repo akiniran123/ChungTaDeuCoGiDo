9010f3446d8493ba03312215de4eb22cdc732102
nguyentuannghia98 <meobietbay09@gmail.com>
2025-08-05 14:16:35 +0700

    d

--- src/components/Navbar/TopBar.tsx
+++ src/components/Navbar/TopBar.tsx
@@ -2,9 +2,9 @@
 export default function TopBar() {
   return (
     <div className="bg-[#e5e4f2] text-sm px-4 py-2 text-center text-black">
-      🏆 <strong>TITAN FORGED WINS THE BE QUIET! CONTEST!</strong> —{" "}
+      🏆 <strong>Sàn Thương Mại Điện Tử Dành Cho Người Mua Thông Thái!</strong> —{" "}
       <a href="#" className="underline hover:text-blue-700">
-        See the silent beast here
+        Nơi mua sắm đáng tin cậy của bạn!
       </a>
     </div>
   );

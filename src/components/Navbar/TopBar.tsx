2b0d32062780e48d364088e84a7dbb1a32f40de5
akiniran123 <69475298+akiniran123@users.noreply.github.com>
2025-08-11 10:37:47 +0700

    Update TopBar.tsx

--- src/components/Navbar/TopBar.tsx
+++ src/components/Navbar/TopBar.tsx
@@ -1,11 +1,12 @@
 'use client';
 export default function TopBar() {
   return (
-    <div className="bg-[#e5e4f2] text-sm px-4 py-2 text-center text-black">
-      🏆 <strong>TITAN FORGED WINS THE BE QUIET! CONTEST!</strong> —{" "}
-      <a href="#" className="underline hover:text-blue-700">
-        See the silent beast here
-      </a>
-    </div>
+    <div className="bg-[#f5f5f5] text-sm px-4 py-2 text-center text-black">
+  🏆 <strong>Sàn Thương Mại Điện Tử Dành Cho Người Mua Thông Thái!</strong> —{" "}
+  <a href="#" className="underline hover:text-blue-700">
+    Nơi mua sắm đáng tin cậy của bạn!
+  </a>
+</div>
+
   );
 }

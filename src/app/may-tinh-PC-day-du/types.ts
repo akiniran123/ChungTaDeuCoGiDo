34532412e0f6896977e2fb1c025a9c92c423ed51
nguyentuannghia98 <meobietbay09@gmail.com>
2025-09-26 19:53:29 +0700

    d

--- src/app/may-tinh-PC-day-du/types.ts
+++ src/app/may-tinh-PC-day-du/types.ts
@@ -6,8 +6,14 @@ export type Product = {
   gpu: string
   ram: string
   name: string
-  price: number // VND as number
+  price: number
   description: string
+  shortDesc?: string
   images: string[]
   stock?: number
+  seller?: {
+    id: number // 👈 thêm nếu bạn đang dùng
+    name: string
+    avatar: string // 👈 sửa lại nếu bạn viết nhầm thành 'antar'
+  }
 }

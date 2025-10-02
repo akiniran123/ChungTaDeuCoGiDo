27f1d52da96e5ceb4dd372e41d654d7bf201143b
akiniran123 <69475298+akiniran123@users.noreply.github.com>
2025-10-02 15:15:45 +0700

    git

--- src/lib/supabase/serverClient.ts
+++ src/lib/supabase/serverClient.ts
@@ -1,6 +1,13 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import type { Database } from '@/types/supabase';
 
-// ✅ Export sẵn một instance để dùng lại
-export const supabase = createServerComponentClient<Database>({ cookies });
+import { NextResponse } from 'next/server'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import type { Database } from '@/types/supabase'
+
+export async function GET() {
+  const supabase = createServerComponentClient<Database>({ cookies })
+
+  await supabase.auth.signOut()
+
+  return NextResponse.redirect(new URL('/', process.env.NEXT_PUBLIC_SITE_URL))
+}

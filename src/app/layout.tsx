c7a4130ef11f3600a0e7ad76621e1dec31d8bfd6
nguyentuannghia98 <meobietbay09@gmail.com>
2025-11-11 22:11:06 +0700

    d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-white text-black min-h-screen flex flex-col">
+      <body className="bg-white text-black min-h-screen flex flex-col -mt-8">
+        {/* -mt-8 = kéo toàn bộ nội dung lên mạnh hơn (~2rem) */}
         <ClientRoot>{children}</ClientRoot>
       </body>
     </html>

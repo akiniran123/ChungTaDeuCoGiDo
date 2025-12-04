ea438897277c3a2d6be6d9e690f7a71223a316be
akiniran123 <69475298+akiniran123@users.noreply.github.com>
2025-12-04 14:48:23 +0700

    ducnh

--- src/components/Trang_chu/pc/SidebarRight.tsx
+++ src/components/Trang_chu/pc/SidebarRight.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function SidebarRightCard() {
   return (
-    <aside className="fixed top-[96px] right-[28px] z-30 w-[260px] h-[calc(100vh-96px)]">
-      <div className="h-full rounded-lg bg-white border border-gray-200 shadow-sm overflow-hidden">
+    <aside className="fixed top-[20px] right-[20px] z-30 w-[260px] h-[calc(100vh-20px)]">
+      <div className="h-full rounded-4xl bg-white border border-gray-200 shadow-sm overflow-hidden">
         {/* Nội dung sẽ thêm sau, hiện tại để trống */}
       </div>
     </aside>

23da6d137573221aa49b77127c17e5ca934dadf9
akiniran123 <69475298+akiniran123@users.noreply.github.com>
2025-12-12 22:08:24 +0700

    ducnash

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,4 +1,3 @@
-// app/profile/[id]/page.tsx
 import OtherUserProfileClient from "@/components/OtherUserProfileClient/ProfileClient";
 
 export default function Page({ params }: { params: { id: string } }) {
